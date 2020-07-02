6f8a3805571c23fddae4fa0d447f79e075978a04
jose <josegg1897@gmail.com>
2020-07-02 15:16:26 -0400

    Fix menu button mobile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { mount, render, createElement, insertionJsx } from "insertion";
+import { mount, render, createElement, DidMount, insertionJsx } from "insertion";
 
 import "./css/style.css";
 import App from "./components/main";
+import script from './scripts/main'
 
 const app = createElement(insertionJsx(App, null));
 
