2abe49f1c0b4b30dcf4ef81ac9f5dd0f9ce9de2b
jose <josegg1897@gmail.com>
2020-07-04 01:58:46 -0400

    Add team component

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -9,19 +9,23 @@ import ShowCase from "./showcase.jsx";
 import Project from "./projects.jsx";
 import Error404 from "./404.jsx";
 import About from "./about.jsx";
+import Team from "./Team.jsx";
 
 function App() {
   return (
     <Fragment>
       <Router>
         <Switch>
+          <Navigation />
           <Route exact path="/">
             <div className="container">
-              <Navigation />
               <ShowCase />
               <Project />
               <About />
             </div>
+          </Route>
+          <Route path="/team">
+              <Team />
           </Route>
 
           <Route path="*">
