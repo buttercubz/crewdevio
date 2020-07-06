e97c657fd940af99220f7ba89722703b3ea4a4b8
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-07-06 14:55:51 -0400

    markdown o=preview and preloader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React, { Fragment } from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Navigation from "./navigation";
+import Markdown from "./markdown";
 import ShowCase from "./showcase";
-import Project from "./projects";
 import Notfound from "./Notfound";
+import Project from "./projects";
 import About from "./about";
 import Team from "./Team";
 
@@ -18,10 +18,16 @@ function App() {
           <Route path="/team">
             <Team />
           </Route>
+
+          <Route path="/projects/:repo" component={Markdown} />
+
+          <Route path="/projects">
+            <Project />
+          </Route>
           <Route exact path="/">
             <div className="container">
               <ShowCase />
-              <Project />
+              <Project two={true} title="two of our" />
               <About />
             </div>
           </Route>
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import trexLogo from "../images/trex_logo.png";
 import importqlLogo from "../images/importql_logo.png";
 
@@ -7,7 +7,7 @@ const projectList = [
   {
     name: "Trex",
     description:
-      "is a Package management for deno similar to npm but maintaining the deno philosophy. packages are cached and only one import_map.json file is generated.",
+      "is a Package management for deno similar to npm but maintaining the deno philosophy.",
     url: "https://github.com/crewdevio/Trex",
     imgPath: trexLogo,
   },
@@ -17,24 +17,26 @@ const projectList = [
     url: "https://github.com/crewdevio/importql",
     imgPath: importqlLogo,
   },
-];
+].sort(() => Math.random() * 0.5);
 
-function Projects() {
+function Projects({ two, title }) {
+  const list = two ? [projectList[0], projectList[1]] : projectList;
   return (
     <section>
       <h1 className="projects-title">
-        Our <span className="color-blue">Projects</span>
+        {title ? title : "Our"}
+        <span className="color-blue"> Projects</span>
       </h1>
       <div className="projects-container">
-        {projectList.map(({ description, imgPath, name, url }, index) => {
+        {list.map(({ description, imgPath, name }, index) => {
           return (
             <div className="project-card" key={index}>
               <img src={imgPath} className="project-logo" alt={index} />
               <h3 className="project-name">{name}</h3>
               <p className="project-desc">{description}</p>
-              <a href={url} className="project-link" target="blank">
-                visited out github repo
-              </a>
+              <Link className="project-link" to={`/projects/${name}`}>
+                View project
+              </Link>
             </div>
           );
         })}
